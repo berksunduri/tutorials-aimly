c9608397fa8df72c4255d00a28a843264cf9061c
Ololade <lolly@gmail.com>
2024-10-25 19:29:14 +0200

    Added thirdWeek

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import firstWeek from './data/firstWeek.json'
 import secondWeek from './data/secondWeek.json'
-// import thirdWeek from './data/thirdWeek.json'
+import thirdWeek from './data/thirdWeek.json'
 // import fourthWeek from './data/fourthWeek.json'
 // import fifthWeek from './data/fifthWeek.json'
 // import sixthWeek from './data/sixthWeek.json'
@@ -19,7 +19,7 @@ import { useTheme } from "./components/theme-provider"
 const units = [
   { title: "Unit 1", data: firstWeek },
   { title: "Unit 2", data: secondWeek },
-  // { title: "Unit 3", data: thirdWeek },
+  { title: "Unit 3", data: thirdWeek },
   // { title: "Unit 4", data: fourthWeek },
   // { title: "Unit 5", data: fifthWeek },
   // { title: "Unit 6", data: sixthWeek },
